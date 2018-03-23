969582b8ea0e4b73e3b3918dc579bb09cffaf55f
Gabriel Santos <gabriel.santos@foster.com.br>
2018-03-23 18:50:40 -0300

    Initial Commit

---
+++ BitHead.SweetHome/BitHead.SweetHome.Shared/Repos/Imovel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitHead.SweetHome.Shared.Repos
+{
+    public class Imovel
+    {
+        public async Task<bool> AddImovelAsync(Database.Imoveis imovel)
+        {
+            try
+            {
+                using (var ctx = new Database.SweetHomeEntities())
+                {
+                    ctx.Imoveis.Add(imovel);
+                    await ctx.SaveChangesAsync();
+                    return true;
+                }
+            }
+            catch (Exception erro)
+            {
+                var idErro = Guid.NewGuid();
+                //TODO: Log mensagem de erro
+                throw new Exception("Ocorreu um problema ao adicionar o imóvel. cod: " + idErro);
+            }
+        }
+
+        public async Task<IEnumerable<Database.Imoveis>> GetImoveisAsync()
+        {
+            try
+            {
+                using (var ctx = new Database.SweetHomeEntities())
+                {
+                    return await Task.FromResult(ctx.Imoveis.ToList());
+                }
+            }
+            catch (Exception erro)
+            {
+                var idErro = Guid.NewGuid();
+                //TODO: Log mensagem de erro
+                throw new Exception("Ocorreu um problema ao recuperar a lista de imóveis. cod: " + idErro);
+            }
+        }
+
+        public async Task<Database.Imoveis> GetImovelAsync(Guid id)
+        {
+            try
+            {
+                using (var ctx = new Database.SweetHomeEntities())
+                {
+                    return await ctx.Imoveis.FindAsync(id);
+                }
+            }
+            catch (Exception erro)
+            {
+                var idErro = Guid.NewGuid();
+                //TODO: Log mensagem de erro
+                throw new Exception("Ocorreu um problema ao recuperar a lista de imóveis. cod: " + idErro);
+            }
+        }
+    }
+}
---
+++ BitHead.SweetHome/BitHead.SweetHome.WebAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BitHead.SweetHome.WebAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BitHead.SweetHome.WebAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6faf5ea7-d165-4d9c-a102-759b1f107d8d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
